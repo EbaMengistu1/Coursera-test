24b63c5da051eaaae8fd89ee5bbd4af1f62c39ab
Eba mengistu <barkotka@gmail.com>
2022-07-22 02:20:36 +0300

    example

---
+++ Lecture52/js/script2.js
@@ -0,0 +1,19 @@
+(function (window) {
+	var johnGreeter = {};
+	johnGreeter.name = "John";
+	var greeting = "Hi "
+	johnGreeter.sayHi = function () {
+	console.log(greeting + johnGreeter.name);
+	}
+
+	window.johnGreeter = johnGreeter;
+
+})(window);
+
+
+var johnGreeter = {};
+johnGreeter.name = "John";
+var greeting = "Hi "
+johnGreeter.sayHi = function () {
+	console.log(greeting + johnGreeter.name);
+}
